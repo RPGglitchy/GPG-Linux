93024f47357b062b54f550a4cce118883794b1e7
Chari Karipidis <chari.sarutobi@gmail.com>
2012-05-25 13:22:35 +0200

    aanpassen van script en afwerking

--- Bash-Script/Mailreviewer.sh
+++ Bash-Script/Mailreviewer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo '                            ____    ____              ____               ____  ____'		
-echo '|\    /|    /\    |  |     |    |  |      \      / | |     \          / |     |    |'
-echo '| \  / |   /  \   |  |     |____|  |____   \    /  | |____  \        /  |____ |____|'
-echo '|  \/  |  /----\  |  |     |   \   |        \  /   | |       \  /\  /   |     |   \'
-echo '|      | /      \ |  |____ |    \  |____     \/    | |____    \/  \/    |____ |    \'
+echo '                            ____    ___              ___               ___  ____'		
+echo '|\    /|    /\    | |     |    |  |     \      / | |    \          / |    |    |'
+echo '| \  / |   /  \   | |     |____|  |___   \    /  | |___  \        /  |___ |____|'
+echo '|  \/  |  /----\  | |     |   \   |       \  /   | |      \  /\  /   |    |   \'
+echo '|      | /      \ | |____ |    \  |___     \/    | |___    \/  \/    |___ |    \'
 echo ' '
 
 
@@ -17,13 +17,13 @@ while true; do
 	
 	echo 'Zijn de gegevens correct?'
 	echo '========================='
-	echo $aanspreking $naam $voornaam
+	echo $aanspreking $voornaam $naam
 	echo 'Het e-mail adres: ' $email
 	echo 'Het pad naar het .tex bestand: ' $pad
 	echo '========================='
-	read -p '[J/N]' correct
+	read -p '[j/n]	' correct
 
-	if [[ "$correct" == "J" ] -o [ "$correct" == "j" ]]
+	if [[ "$correct" == "j" ]]
 		then break;
 	fi
 done
@@ -31,24 +31,37 @@ done
 echo 'Om fouten te vermijden wordt er een backup gemaakt van de huidige .tex file.'
 cp "$pad/GPG.tex" "$pad/GPGbackup.tex"
 
-sed -r "s/\rfoot{}/\rfoot{$naam $voornaam }/" "$pad/GPG.tex" > "$pad/GPG.txt" && mv "$pad/GPG.txt" "$pad/GPG.tex"
+sed -r "s/xname/$naam/" "$pad/GPG.tex" > "$pad/GPG.txt" && mv "$pad/GPG.txt" "$pad/GPG.tex"
+sed -r "s/xfirstname/$voornaam/" "$pad/GPG.tex" > "$pad/GPG.txt" && mv "$pad/GPG.txt" "$pad/GPG.tex"
 
-zip -r "$pad/KaripidisChari_$naam$voornaam.zip" $pad > "$pad/zip.txt"
+cd $pad
+pdflatex GPG.tex
+cd ~
+
+echo ' '
+echo 'De naam is succesvol toegevoegd aan het .pdf bestand.'
+echo ' '
+
+mv "$pad/GPG.tex" "$pad/$voornaam$naam.pdf"
 
 read -p 'Er opent, bij het drukken op enter, een tekstverwerker om een bericht toe te voegen in uw mail.' pause
 gedit "$pad/tempMail.txt"
 
 read -p 'geef een onderwerp voor de mail: ' onderwerp
 echo "De mail wordt vestuurd naar: $email."
-mutt -s $onderwerp $email -a "$pad/KaripidisChari_$naam$voornaam.zip" < "$pad/tempMail.txt"
+mutt -s $onderwerp $email -a "$pad/$voornaam$naam.pdf" < "$pad/tempMail.txt"
 echo "De mail is succesvol verzonden naar $email."
 
-rm "$pad/zip.txt" "$pad/KaripidisChari_$naam$voornaam.zip" "$pad/tempMail.txt"
+rm "$pad/zip.txt" "$pad/tempMail.txt"
 
-read -p 'Wenst u het backup bestand terug te plaatsen?' backup
+read -p 'Wenst u het backup bestand terug te plaatsen? [j/n]	' backup
 
-if [[ "$backup" == "J" ] -o [ "$backup" == "j" ]]
+if [[ "$backup" == "j" ]]
 	then mv $pad/GPGbackup.tex $pad/GPG.tex
 fi
 
+echo ''
+echo 'Bedankt voor het gebruik van mailreviewer.'
+echo ''
+read -p 'Live long and prosper...' pause
 
--- LaTeX/Mailreviewer.sh
+++ LaTeX/Mailreviewer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo '                            ____    ___              ___               ___  ____'		
-echo '|\    /|    /\    |  |     |    |  |     \      / | |    \          / |    |    |'
-echo '| \  / |   /  \   |  |     |____|  |___   \    /  | |___  \        /  |___ |____|'
-echo '|  \/  |  /----\  |  |     |   \   |       \  /   | |      \  /\  /   |    |   \'
-echo '|      | /      \ |  |____ |    \  |___     \/    | |___    \/  \/    |___ |    \'
+echo '|\    /|    /\    | |     |    |  |     \      / | |    \          / |    |    |'
+echo '| \  / |   /  \   | |     |____|  |___   \    /  | |___  \        /  |___ |____|'
+echo '|  \/  |  /----\  | |     |   \   |       \  /   | |      \  /\  /   |    |   \'
+echo '|      | /      \ | |____ |    \  |___     \/    | |___    \/  \/    |___ |    \'
 echo ' '
 
 
@@ -17,7 +17,7 @@ while true; do
 	
 	echo 'Zijn de gegevens correct?'
 	echo '========================='
-	echo $aanspreking $naam $voornaam
+	echo $aanspreking $voornaam $naam
 	echo 'Het e-mail adres: ' $email
 	echo 'Het pad naar het .tex bestand: ' $pad
 	echo '========================='
@@ -31,23 +31,28 @@ done
 echo 'Om fouten te vermijden wordt er een backup gemaakt van de huidige .tex file.'
 cp "$pad/GPG.tex" "$pad/GPGbackup.tex"
 
-sed -r "s/xname/$naam $voornaam/" "$pad/GPG.tex" > "$pad/GPG.txt" && mv "$pad/GPG.txt" "$pad/GPG.tex"
+sed -r "s/xname/$naam/" "$pad/GPG.tex" > "$pad/GPG.txt" && mv "$pad/GPG.txt" "$pad/GPG.tex"
+sed -r "s/xfirstname/$voornaam/" "$pad/GPG.tex" > "$pad/GPG.txt" && mv "$pad/GPG.txt" "$pad/GPG.tex"
 
 cd $pad
 pdflatex GPG.tex
 cd ~
 
-zip -r "$pad/KaripidisChari_$naam$voornaam.zip" $pad > "$pad/zip.txt"
+echo ' '
+echo 'De naam is succesvol toegevoegd aan het .pdf bestand.'
+echo ' '
+
+mv "$pad/GPG.pdf" "$pad/$voornaam$naam.pdf"
 
 read -p 'Er opent, bij het drukken op enter, een tekstverwerker om een bericht toe te voegen in uw mail.' pause
 gedit "$pad/tempMail.txt"
 
 read -p 'geef een onderwerp voor de mail: ' onderwerp
 echo "De mail wordt vestuurd naar: $email."
-mutt -s $onderwerp $email -a "$pad/KaripidisChari_$naam$voornaam.zip" < "$pad/tempMail.txt"
+mutt -s $onderwerp $email -a "$pad/$voornaam$naam.pdf" < "$pad/tempMail.txt"
 echo "De mail is succesvol verzonden naar $email."
 
-rm "$pad/zip.txt" "$pad/KaripidisChari_$naam$voornaam.zip" "$pad/tempMail.txt"
+rm "$pad/zip.txt" "$pad/tempMail.txt" "$pad/$voornaam$naam.pdf"
 
 read -p 'Wenst u het backup bestand terug te plaatsen? [j/n]	' backup
 
@@ -55,4 +60,10 @@ if [[ "$backup" == "j" ]]
 	then mv $pad/GPGbackup.tex $pad/GPG.tex
 fi
 
+echo ''
+echo 'Bedankt voor het gebruik van mailreviewer.'
+echo ''
+read -p 'Live long and prosper...' pause
+
+exit
 
